02ce7065f369f0d1f3bc52350aae71355ebeff42
ximeng <ximenglau@outlook.con>
2021-07-14 15:56:45 +0800

    c# async in fact still wait when check the value before it returns

---
+++ testAsync/testAsync/testAsync/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace testAsync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Task<int> value = calleeAsync(1);
+            Console.WriteLine("Value: {0}", value.Result);
+        }
+        private static async Task<int> calleeAsync(int a) 
+        {
+            await Task.Run(() => Thread.Sleep(5000)
+            ) ;
+            return a; 
+        }
+
+    }
+}
