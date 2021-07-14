99f5d919d963541124a006c06239b64455f569cc
ximeng <ximenglau@outlook.con>
2021-07-14 16:05:40 +0800

    c# async

---
+++ testAsync/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace testAsync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Task<int> value = calleeAsync(1);
+            Console.WriteLine("Value: {0}", value.Result);
+        }
+        private static async Task<int> calleeAsync(int a) 
+        {
+            Console.WriteLine("Hello calleeAsync!");
+            await Task.Run(() => Thread.Sleep(5000)
+            ) ;
+            Console.WriteLine("Hello back!");
+            return a; 
+        }
+
+    }
+}
